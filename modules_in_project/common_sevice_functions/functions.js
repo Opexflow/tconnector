920120ff024011b50b61cf340a50465c0c18fa8e
Rahul Prajapati <rohngonnarock@gmail.com>
2022-04-05 12:47:52 +0530

    Lint error fix

--- modules_in_project/common_sevice_functions/functions.js
+++ modules_in_project/common_sevice_functions/functions.js
@@ -17,80 +17,69 @@
  *
  * @return string
  */
-function functionContractString(
-    year,
-    month,
-    day,
-) {
+function functionContractString(year, month, day) {
     const contrakt = 'Si';
-    let contractsLetter = '';
-    let lastNumberInYear = Number(year.substring(3));
+
+    const lastNumberInYear = Number(year.substring(3));
 
     month = Number(month);
     day = Number(day);
-    if (
-        month === 12 &&
-        day >= 15
-    ) {
+    const { contractsLetter, lastNumberInYearNew } = getContractedLetter(
+        month,
+        day,
+        lastNumberInYear,
+    );
+
+    return contrakt + contractsLetter + lastNumberInYearNew;
+}
+
+function getSubContractedLetter(month, day) {
+    let contractsLetter = '';
+
+    if (month < 3 || (month === 3 && day < 15)) {
         contractsLetter = 'H';
-        lastNumberInYear++;
-        if (lastNumberInYear === 10) {
-            lastNumberInYear = 0;
-        }
-    } else
-    if (
-        month < 3 ||
-        (
-            month === 3 &&
-            day < 15
-        )
-    ) {
-        contractsLetter = 'H';
-    } else
-    if (
-        (
-            month === 3 &&
-            day >= 15
-        ) ||
-        month === 4 ||
-        month === 5 ||
-        (
-            month === 6 &&
-            day < 15
-        )
+    } else if (
+        (month === 3 && day >= 15) ||
+    month === 4 ||
+    month === 5 ||
+    (month === 6 && day < 15)
     ) {
         contractsLetter = 'M';
-    } else
-    if (
-        (
-            month === 6 &&
-            day >= 15
-        ) ||
-        month === 7 ||
-        month === 8 ||
-        (
-            month === 9 &&
-            day < 15
-        )
+    } else if (
+        (month === 6 && day >= 15) ||
+    month === 7 ||
+    month === 8 ||
+    (month === 9 && day < 15)
     ) {
         contractsLetter = 'U';
-    } else
-    if (
-        (
-            month === 9 &&
-            day >= 15
-        ) ||
-        month === 10 ||
-        month === 11 ||
-        (
-            month === 12 &&
-            day < 15
-        )
+    } else if (
+        (month === 9 && day >= 15) ||
+    month === 10 ||
+    month === 11 ||
+    (month === 12 && day < 15)
     ) {
         contractsLetter = 'Z';
     }
 
-    return contrakt + contractsLetter + lastNumberInYear;
+    return contractsLetter;
+}
+
+function getContractedLetter(month, day, lastNumber) {
+    let contractsLetter = '';
+    let lastNumberInYear = lastNumber;
+
+    if (month === 12 && day >= 15) {
+        contractsLetter = 'H';
+        lastNumberInYear++;
+        lastNumberInYear = lastNumberInYear === 10 ? 0 : lastNumber;
+    }
+    let contractsLetterNew = getSubContractedLetter(month, day);
+
+    contractsLetterNew = contractsLetterNew ?
+        contractsLetterNew :
+        contractsLetter;
+
+    return { contractsLetterNew, lastNumberInYearNew: lastNumberInYear };
 }
 
 // #endregion
