a145b2b9d68e3aeb5ad6d51646de6416fcdd4e4f
pskucherov <pskucherov@yandex.ru>
2022-08-22 16:45:39 +0300

    shares n futures

--- tconnector.js
+++ tconnector.js
@@ -81,168 +81,303 @@ const dllFunctions = {
 
 // let i = 0;
 
-class TConnector {
-    constructor(hft = false, host = 'tr1.finam.ru', port = 3900) {
-        this.isHFT = Boolean(hft);
-        this.host = host;
-        this.port = port;
+try {
+    class TConnector {
+        constructor(hft = false, host = 'tr1.finam.ru', port = 3900) {
+            try {
+                this.isHFT = Boolean(hft);
+                this.host = host;
+                this.port = port;
 
-        const dllFile = config.dllFiles[this.isHFT ? 'Hft' : 'NotHft'];
+                const dllFile = config.dllFiles[this.isHFT ? 'Hft' : 'NotHft'];
 
-        this.sdk = ffi.Library(
-            path.resolve(__dirname, dllFile),
-            dllFunctions,
-        );
+                this.sdk = ffi.Library(
+                    path.resolve(__dirname, dllFile),
+                    dllFunctions,
+                );
 
-        this.client = false;
+                this.clients = [];
+                this.client = false;
+                this.accountIdSelected;
 
-        this.errorMessage;
-    }
+                this.errorMessage;
 
-    async connect(login, password) {
-        this.inProgress = true;
-        this.token = login;
-
-        const ffiCallback = ffi.Callback(
-            ffi.types.bool,
-            [ref.refType(ffi.types.CString)],
-            msg => {
-                // callback(ref.readCString(msg, 0), this.isHFT);
-                const tString = ref.readCString(msg, 0);
-                const t = JSON.parse(xml2json.toJson(tString));
-
-                if (t.server_status && t.server_status.connected === 'error') {
-                    this.errorMessage = t.server_status['$t'];
-
-                    // {"server_status":{"connected":"error","$t":"Неверный идентификатор, пароль или Touch Memory"}}
-                    // {"server_status":{"sys_ver":"629","build":"18","server_tz":"Russian Standard Time","id":"6","connected":"true"}}
-                }
-
-                if (!t.markets &&
-                    !t.candlekinds &&
-                    !t.securities &&
-                    !t.pits &&
-                    !t.sec_info_upd &&
-                    !t.boards
-                ) {
-                    console.log(tString, t); // eslint-disable-line no-console
-                }
-
-                // else {
-                //     ++i;
-                //     if (!(i % 1000)) {
-                //         console.log('conn', i)
-                //     }
-                // }
-
-                if (t.client) {
-                    // client: {
-                    //     id: '7683898',
-                    //     remove: 'false',
-                    //     market: '4',
-                    //     currency: 'RUB',
-                    //     type: 'spot',
-                    //     forts_acc: '7683898'
-                    //   }
-                    this.client = t.client;
-                }
-
-                if (msg !== undefined) {
-                    this.sdk.FreeMemory(msg);
-                }
-
-                return null;
-            },
-        );
-
-        process.on('exit', function() {
-            this.disconnect();
-            const x = ffiCallback;
-        });
-
-        const promise = new Promise((resolve, reject) => {
-            resolve(
-                this.sdk.Initialize(
-                    path.resolve(__dirname, `log/${this.isHFT ? 'hft' : 'default'}`),
-                    1,
-                ),
-            );
-        });
-
-        try {
-            this.sdk.SetCallback(ffiCallback);
-
-            // if (this.isHFT) {
-            //     this.sdk.SetCallback(ffiCallback);
-            // } else {
-            //     this.sdk.SetCallback(ffiCallback);
-            // }
-
-            const myXMLConnectString = `${'<command id="connect">' + '<login>'}${login}</login>` +
-                `<password>${password}</password>` +
-                `<host>${this.host}</host>` +
-                `<port>${this.port}</port>` +
-                '<language>en</language>' +
-                '<autopos>false</autopos>' +
-                '<session_timeout>200</session_timeout>' +
-                '<request_timeout>20</request_timeout>' +
-                closeCommandStr;
-
-            this.sdk.SendCommand(myXMLConnectString);
-
-            // this.checkServerStatusInterval();
-        } catch (err) {
-            // console.log(`Promise ${this.isHFT} catch ${err}`);
+                this.shares = [];
+                this.futures = [];
+            } catch (e) {
+                console.log(e); // eslint-disable-line no-console
+            }
         }
 
-        return null;
-    }
+        async connect(login, password, accountId) { // eslint-disable-line sonarjs/cognitive-complexity
+            this.inProgress = true;
+            this.token = login;
+            this.accountIdSelected = accountId;
 
-    // checkServerStatusInterval() {
-    //     this.checkInterval = setInterval(() => {
-    //         this.checkServerStatus();
-    //     }, 5000);
-    // }
+            const ffiCallback = ffi.Callback(
+                ffi.types.bool,
+                [ref.refType(ffi.types.CString)],
+                msg => {
+                    try {
+                        // callback(ref.readCString(msg, 0), this.isHFT);
+                        const tString = ref.readCString(msg, 0);
+                        const t = JSON.parse(xml2json.toJson(tString));
 
-    getClientId() {
-        return this.client?.id || undefined;
-    }
+                        if (t.server_status && t.server_status.connected === 'error') {
+                            this.errorMessage = t.server_status['$t'];
 
-    checkServerStatus() {
-        const command = 'server_status';
-        const result = this.sdk.SendCommand(`<command id="${command}"/>`);
-        const r = JSON.parse(xml2json.toJson(result));
-        let answer = {};
+                            // {"server_status":{"connected":"error","$t":"Неверный идентификатор, пароль или Touch Memory"}}
+                            // {"server_status":{"sys_ver":"629","build":"18","server_tz":"Russian Standard Time","id":"6","connected":"true"}}
+                        }
 
-        if (r.result.success === 'true') {
-            this.connected = true;
+                        if (!t.markets &&
+                            !t.candlekinds &&
 
-            if (this.errorMessage) {
-                delete this.errorMessage;
+                            // !t.securities &&
+                            !t.pits &&
+                            !t.sec_info_upd &&
+                            !t.boards
+                        ) {
+                            // console.log(t); // eslint-disable-line no-console
+                            // console.log(t.securities.security[0])
+                            // console.log(t.securities.security[0].sectype, t.securities.security[0].board, t.securities.security[0].currency)
+                        }
+
+                        if (t.securities) { //} && t.securities.security[0].currency === 'RUR') {
+                            for (const s of t.securities.security) {
+                                if (s.sectype === 'FUT' && s.currency === 'RUR') {
+                                    this.futures.push({
+                                        ...s,
+                                        ticker: s.seccode,
+                                        figi: s.seccode,
+                                        name: s.shortname,
+                                    });
+                                } else if (s.sectype === 'SHARE' && s.currency === 'RUR') {
+                                    this.shares.push({
+                                        ...s,
+                                        ticker: s.seccode,
+                                        figi: s.seccode,
+                                        name: s.shortname,
+                                    });
+                                }
+
+                                // this.shares = this.shares.concat(t.securities.security);
+                            }
+                        }
+
+                        // else {
+                        //     ++i;
+                        //     if (!(i % 1000)) {
+                        //         console.log('conn', i)
+                        //     }
+                        // }
+
+                        if (t.client) {
+                            // client: {
+                            //     id: '7683898',
+                            //     remove: 'false',
+                            //     market: '4',
+                            //     currency: 'RUB',
+                            //     type: 'spot',
+                            //     forts_acc: '7683898'
+                            //   }
+
+                            this.clients.push(t.client);
+
+                            // this.client = t.client;
+                            // console.log(t.client);
+
+                            if (this.accountIdSelected === t.client.id) {
+                                this.client = { ...t.client };
+                            }
+                        }
+
+                        if (t.mc_portfolio) {
+                            this.portfolioSet(t.mc_portfolio);
+                        }
+
+                        if (msg !== undefined) {
+                            this.sdk.FreeMemory(msg);
+                        }
+
+                        return null;
+                    } catch (e) {
+                        console.log(e); // eslint-disable-line no-console
+                    }
+                },
+            );
+
+            process.on('exit', function() {
+                this.disconnect();
+                const x = ffiCallback;
+            });
+
+            const promise = new Promise((resolve, reject) => {
+                resolve(
+                    this.sdk.Initialize(
+                        path.resolve(__dirname, `log/${this.isHFT ? 'hft' : 'default'}`),
+                        1,
+                    ),
+                );
+            });
+
+            try {
+                this.sdk.SetCallback(ffiCallback);
+
+                // if (this.isHFT) {
+                //     this.sdk.SetCallback(ffiCallback);
+                // } else {
+                //     this.sdk.SetCallback(ffiCallback);
+                // }
+
+                const myXMLConnectString = `${'<command id="connect">' + '<login>'}${login}</login>` +
+                    `<password>${password}</password>` +
+                    `<host>${this.host}</host>` +
+                    `<port>${this.port}</port>` +
+                    '<language>en</language>' +
+                    '<autopos>false</autopos>' +
+                    '<session_timeout>200</session_timeout>' +
+                    '<request_timeout>20</request_timeout>' +
+                    closeCommandStr;
+
+                this.sdk.SendCommand(myXMLConnectString);
+
+                // this.checkServerStatusInterval();
+            } catch (e) {
+                console.log(e); // eslint-disable-line no-console
             }
-        } else {
-            this.connected = false;
-            answer = {
-                ...r.result,
+
+            return null;
+        }
+
+        // checkServerStatusInterval() {
+        //     this.checkInterval = setInterval(() => {
+        //         this.checkServerStatus();
+        //     }, 5000);
+        // }
+
+        getClientId() {
+            return this.client?.id || undefined;
+        }
+
+        getClientUnion() {
+            return this.client?.union || undefined;
+        }
+
+        setSelectedAccountId(id) {
+            try {
+                for (const c of this.clients) {
+                    if (c.id === id) {
+                        this.client = { ...c };
+                        break;
+                    }
+                }
+            } catch (e) {
+                console.log(e); // eslint-disable-line no-console
+            }
+        }
+
+        checkServerStatus() {
+            try {
+                const command = 'server_status';
+                const result = this.sdk.SendCommand(`<command id="${command}"/>`);
+                const r = JSON.parse(xml2json.toJson(result));
+                let answer = {};
+
+                if (r.result.success === 'true') {
+                    this.connected = true;
+
+                    if (this.errorMessage) {
+                        delete this.errorMessage;
+                    }
+
+                    if (this.getClientId()) {
+                        this.getPortfolioSend();
+                    }
+                } else {
+                    this.connected = false;
+                    answer = {
+                        ...r.result,
+                    };
+                }
+
+                return {
+                    ...answer,
+                    accountId: this.getClientId(),
+                    errorMessage: this.errorMessage,
+                    connected: this.connected,
+                };
+            } catch (e) {
+                console.log(e); // eslint-disable-line no-console
+            }
+        }
+
+        getClients() {
+            return this.clients;
+        }
+
+        getPortfolioSend() {
+            try {
+                const clientId = this.getClientId();
+                const union = this.getClientUnion();
+
+                if (!clientId && !union) {
+                    return;
+                }
+
+                const user = union ? `union="${union}"` : `client="${clientId}"`;
+
+                const myXMLConnectString = `<command id="get_mc_portfolio" ${user} 
+                    currency="true" asset="true" money="true" depo="true"
+                    registers="true" maxbs="true"/></command>`;
+
+                this.sdk.SendCommand(myXMLConnectString);
+            } catch (e) {
+                console.log(e); // eslint-disable-line no-console
+            }
+        }
+
+        getPortfolio() {
+            return this.portfolio || {};
+        }
+
+        portfolioSet(p) {
+            this.portfolio = p;
+        }
+
+        getShares() {
+            return {
+                shares: {
+                    instruments: this.shares,
+                },
             };
         }
 
-        return {
-            ...answer,
-            accountId: this.getClientId(),
-            errorMessage: this.errorMessage,
-            connected: this.connected,
-        };
+        getFutures() {
+            return {
+                futures: {
+                    instruments: this.futures,
+                },
+            };
+        }
+
+        disconnect() {
+            try {
+                this.inProgress = false;
+                this.checkInterval && clearInterval(this.checkInterval);
+
+                this.sdk.SendCommand(
+                    '<command id="disconnect"/>',
+                );
+
+                this.sdk.UnInitialize();
+            } catch (e) {
+                console.log(e); // eslint-disable-line no-console
+            }
+        }
     }
 
-    disconnect() {
-        this.inProgress = false;
-        this.checkInterval && clearInterval(this.checkInterval);
-
-        this.sdk.SendCommand(
-            '<command id="disconnect"/>',
-        );
-    }
+    module.exports.TConnector = TConnector;
+} catch (e) {
+    console.log(e); // eslint-disable-line no-console
 }
-
-module.exports.TConnector = TConnector;
